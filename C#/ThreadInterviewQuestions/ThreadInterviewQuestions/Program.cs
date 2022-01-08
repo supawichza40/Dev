a7453bbe129c11017d1cc1a09e94c68a3bf0c529
Supawichza40 <supawichza@gmail.com>
2022-01-08 20:43:28 +0000

    Completed way to compare value inside a list not by reference.

---
+++ C#/ThreadInterviewQuestions/ThreadInterviewQuestions/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ThreadInterviewQuestions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //How to pass parameter to a function when using multithreading
+            Thread t = new Thread(()=>PrintOneToHundread(10));
+            t.Start();
+            t.Join();
+            ParameterizedThreadStart ts = new ParameterizedThreadStart(PrintOneToHundread);
+            Thread t2 = new Thread(ts);
+            t2.Start(10);
+        }
+        public static void PrintOneToHundread(object val)
+        {
+            for (int i = 0; i < ((int)val); i++)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
